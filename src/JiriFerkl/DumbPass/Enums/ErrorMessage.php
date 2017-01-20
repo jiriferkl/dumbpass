c0f1de88ef80fbac72b73c052207b8cc1e6951c5
Jiří Ferkl <jiri.ferkl@protonmail.com>
2017-01-20 16:02:09 +0100

    Add: Enums

---
+++ src/JiriFerkl/DumbPass/Enums/ErrorMessage.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace JiriFerkl\DumbPass\Enums;
+
+use Consistence\Enum\Enum;
+
+/**
+ * Class ErrorMessage
+ *
+ * @package JiriFerkl\DumbPass\Enums
+ */
+class ErrorMessage extends Enum //TODO maybe final
+{
+
+	const LENGTH  = 'length';
+	const NUMERIC = 'numeric';
+	const CAPITAL = 'capital';
+	const SPECIAL = 'special';
+
+}
---
+++ src/JiriFerkl/DumbPass/Enums/Localization.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+namespace JiriFerkl\DumbPass\Enums;
+
+use Consistence\Enum\Enum;
+
+/**
+ * Class Localization
+ *
+ * @package JiriFerkl\DumbPass\Enums
+ */
+class Localization extends Enum //TODO maybe final
+{
+
+	const CZ = 'cz';
+	const EN = 'en';
+
+}
