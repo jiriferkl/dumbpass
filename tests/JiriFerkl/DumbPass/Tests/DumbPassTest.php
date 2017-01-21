bf685024a875c4ebd97ec008152bf83cccd843cb
Jiří Ferkl <jiri.ferkl@protonmail.com>
2017-01-21 15:33:36 +0100

    Add: DumbPassTest sceleton

---
+++ tests/JiriFerkl/DumbPass/Tests/DumbPassTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+namespace JiriFerkl\DumbPass\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DumbPassTest
+ *
+ * @package JiriFerkl\DumbPass\Tests
+ */
+final class DumbPassTest extends TestCase
+{
+
+	//TODO
+}
